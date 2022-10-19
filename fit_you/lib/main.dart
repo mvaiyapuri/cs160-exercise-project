027eaac41b0f941bc75569e4c88f9245d8f9a31d
Rebecca Salas <49887988+Rebecca-Salas@users.noreply.github.com>
2022-10-19 15:23:24 -0700

    Update main.dart

--- fit_you/lib/main.dart
+++ fit_you/lib/main.dart
@@ -1,115 +1,273 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const Login());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'Welcome';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: _title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: const Color(0xFFF7D5DB),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(centerTitle: true, title: const Text(_title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25))),
+        body: const MyStatefulWidget(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(70),
+                child: const Text(
+                  'FITYOU',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 70),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Username: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                    borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Password: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              child: const Text('Forgot Password?',),
+            ),
+            Container(
+                height: 80,
+                padding: const EdgeInsets.fromLTRB(90, 0, 90, 0),
+                child: ElevatedButton(
+                  child: const Text('LOGIN', style: TextStyle(fontSize: 30)),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Don\'t have an account?'),
+                TextButton(
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const SignIn()),
+                    );//signup screen
+                  },
+                )
+              ],
             ),
           ],
-        ),
+        ));
+  }
+}
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  static const String _title = 'Register';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: const Color(0xFFF7D5DB),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+        appBar: AppBar(centerTitle: true, title: const Text(_title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25))),
+        body: const MySignUpWidget(),
+      ),
+    );
+  }
+}
+class MySignUpWidget extends StatefulWidget {
+  const MySignUpWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MySignUpWidget> createState() => _MySignUpWidgetState();
+}
+
+class _MySignUpWidgetState extends State<MySignUpWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+            child: const Text(
+              'Sign Up',
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 30
+              )
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 200, 10),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Username: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 200, 10),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'First Name: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 200, 10),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Date of Birth: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 200, 10),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Height: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 200, 40),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.redAccent),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  labelText: 'Weight: ',
+                  labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)
+
+              ),
+            ),
+          ),
+          Container(
+              height: 80,
+              padding: const EdgeInsets.fromLTRB(90, 0, 90, 0),
+              child: ElevatedButton(
+                child: const Text('LOGIN', style: TextStyle(fontSize: 30)),
+                onPressed: () {
+                  print(nameController.text);
+                  print(passwordController.text);
+                },
+              )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Have an account?'),
+              TextButton(
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pop(context); //LogIn
+                },
+              )
+            ],
+          ),
+        ]
+      )
     );
   }
 }
