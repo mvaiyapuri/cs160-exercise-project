ac018789cc4499b94359f98a8c778f153281e622
Dexter Estrada <dexter.estrada99@gmail.com>
2022-12-03 14:51:06 -0800

    Added lists and getWarmups

--- fit_you/lib/provider/workout_provider.dart
+++ fit_you/lib/provider/workout_provider.dart
@@ -9,5 +9,19 @@ class WorkoutProvider with ChangeNotifier {
   //final url = 'http://localhost:5000/';
   final url = 'http://10.0.2.2:5000/';
 
-  
+  static List<Workout> warmups = [];
+  static List<Workout> mainworkouts = [];
+  static List<Workout> cooldowns = [];
+  static List<Workout> recreationals =[];
+
+  Future<void> get getWarmups async{
+    var response;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var userId = prefs.getInt('id');
+    response = await http.get(Uri.parse('${url}warmup/$userId'));
+    List<dynamic> body = json.decode(response.body);
+    warmups = body.map((e) => Workout(
+      id: e['id'], workoutname: e['workoutname'], level: e['level'], description: e['description'], workoutType: e['workoutType'], duration: e['duration']))
+    .toList();
+  }
 }
